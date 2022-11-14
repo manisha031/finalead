dd37670136011fd513937f881a29515225fc52dc
manisha031 <manishareddyjakkula@gmail.com>
2022-11-14 14:06:26 -0600

    new

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
     document.getElementById("grpMembers").style.opacity = "100%";
 }
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
